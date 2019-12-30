2e7c5238c1c4b39e0a7b82a4a394893a4623a98d
Jordan Spangenberg <jordan.spang@gmail.com>
2019-12-29 18:41:57 -0800

    Initial navbar and material ui components

--- src/App.js
+++ src/App.js
@@ -2,24 +2,29 @@ import React from 'react';
 import './App.css';
 import { BrowserRouter as Router, Route, Switch } from 'react-router-dom';
 
+// Components
+import Navbar from './components/Navbar';
+
 // Pages
-import home from './pages/home'
-import login from './pages/login'
-import signup from './pages/signup'
+import home from './pages/home';
+import login from './pages/login';
+import signup from './pages/signup';
 
 function App() {
   return (
     <div className='App'>
       <Router>
-        <Switch>
-          <Route exact path='/' component={home}/>
-          <Route exact path='/login' component={login}/>
-          <Route exact path='/signup' component={signup}/>
-        </Switch>
+        <Navbar />
+        <div className='container'>
+          <Switch>
+            <Route exact path='/' component={home} />
+            <Route exact path='/login' component={login} />
+            <Route exact path='/signup' component={signup} />
+          </Switch>
+        </div>
       </Router>
-
     </div>
-  )
+  );
 }
 
 export default App;
